4d652d2ff39e086d5c260388214fc69a7663ff59
elliottbarnes <elliottnb55@gmail.com>
2024-12-15 22:32:21 -0330

    Initial Commit

---
+++ MusicCloneApp/MusicCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicCloneAppApp.swift
+//  MusicCloneApp
+//
+//  Created by Elliott Barnes on 2024-12-15.
+//
+
+import SwiftUI
+
+@main
+struct MusicCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MusicCloneAppTests/MusicCloneAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  MusicCloneAppTests.swift
+//  MusicCloneAppTests
+//
+//  Created by Elliott Barnes on 2024-12-15.
+//
+
+import Testing
+
+struct MusicCloneAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
